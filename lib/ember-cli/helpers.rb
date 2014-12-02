8cfdb0e0432cb4b9e87c87c7a0ddced26fae1f2e
Pavel Pravosud <pavel@pravosud.com>
2014-12-01 23:25:21 -0500

    Codestyle for helpers

--- lib/ember-cli/helpers.rb
+++ lib/ember-cli/helpers.rb
@@ -1,15 +1,18 @@
 module EmberCLI
   module Helpers
-    # http://stackoverflow.com/a/5471032
-    def self.which(cmd)
-      exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
-      ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
-        exts.each { |ext|
+    extend self
+
+    def which(cmd)
+      exts = ENV.fetch("PATHEXT", ?;).split(?;, -1).uniq
+
+      ENV.fetch("PATH").split(File::PATH_SEPARATOR).each do |path|
+        exts.each do |ext|
           exe = File.join(path, "#{cmd}#{ext}")
           return exe if File.executable?(exe) && !File.directory?(exe)
-        }
+        end
       end
-      return nil
+
+      nil
     end
   end
 end
