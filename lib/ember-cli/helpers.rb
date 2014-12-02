cc2d14b42354a0f87aac3d93bf98d4d956613fbe
Jonathan Jackson <jonathan.jackson1@me.com>
2014-12-01 22:12:34 -0500

    Use tee to split output only if available

---
+++ lib/ember-cli/helpers.rb
@@ -0,0 +1,15 @@
+module EmberCLI
+  module Helpers
+    # http://stackoverflow.com/a/5471032
+    def self.which(cmd)
+      exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+      ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+        exts.each { |ext|
+          exe = File.join(path, "#{cmd}#{ext}")
+          return exe if File.executable?(exe) && !File.directory?(exe)
+        }
+      end
+      return nil
+    end
+  end
+end
