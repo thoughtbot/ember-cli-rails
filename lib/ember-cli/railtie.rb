32d83e42b33a8a113ecac22d38afe0bc85ecb688
Pavel Pravosud <pavel@pravosud.com>
2014-12-10 15:35:12 -0500

    Add a *secret* flag to skip ember compilation
    Fixes #17

--- lib/ember-cli/railtie.rb
+++ lib/ember-cli/railtie.rb
@@ -11,13 +11,20 @@ module EmberCLI
     end
 
     initializer "ember-cli-rails.enable" do
-      EmberCLI.enable! if EmberCLI::Helpers.non_production?
+      EmberCLI.enable! if enable?
     end
 
     rake_tasks do
       require "sprockets/rails/task"
-      EmberCLI::Helpers.override_assets_precompile_task!
+      Helpers.override_assets_precompile_task!
     end
 
+    def enable?
+      !skip_ember_cli_init? && Helpers.non_production?
+    end
+
+    def skip_ember_cli_init?
+      ENV.fetch("SKIP_EMBER", false)
+    end
   end
 end
