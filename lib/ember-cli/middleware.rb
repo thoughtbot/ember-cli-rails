ae7b46ae308302de33ab00e0ffa4a8aaf1994a8d
Jonathan Jackson and Pavel Pravosud <dev+rondale-sc+rwz@hashrocket.com>
2014-12-19 16:54:35 -0500

    Compile ember app only upon request.

--- lib/ember-cli/middleware.rb
+++ lib/ember-cli/middleware.rb
@@ -2,27 +2,49 @@ module EmberCLI
   class Middleware
     def initialize(app)
       @app = app
+      @ember_apps_started = {}
     end
 
     def call(env)
-      enable_ember_cli
-      EmberCLI.wait!
-
-      @app.call(env)
+      request = Rack::Request.new(env)
+      check_ember request
+      @app.call env
     end
 
     private
 
-    def enable_ember_cli
-      @enabled ||= begin
-        if Rails.env.development?
-          EmberCLI.run!
-        else
-          EmberCLI.compile!
-        end
+    def check_ember(request)
+      return unless request.get?
 
-        true
+      if app_name = request.fullpath[ember_assets_path, 1]
+        app = ember_apps.fetch(app_name)
+        prepare_ember_app app
+        app.wait
       end
+    end
+
+    def prepare_ember_app(app)
+      return if @ember_apps_started[app.name]
+      @ember_apps_started.store app.name, true
+
+      if Rails.env.development?
+        app.run
+      else
+        app.compile
+      end
+    end
+
+    def ember_assets_path
+      @ember_assets_path ||= begin
+        prefix = Rails.configuration.assets.prefix
+        ember_apps_names = Regexp.union(*ember_apps.keys)
+
+        Regexp.new("\\A#{prefix}\/(#{ember_apps_names})\/.+")
+      end
+    end
+
+    def ember_apps
+      EmberCLI.configuration.apps
     end
   end
 end
--- lib/ember-cli/view_helpers.rb
+++ lib/ember-cli/view_helpers.rb
@@ -2,12 +2,31 @@ module EmberCLI
   module ViewHelpers
     def include_ember_script_tags(app_name)
       app = EmberCLI.configuration.apps.fetch(app_name)
-      javascript_include_tag *app.exposed_js_assets
+      ember_javascript_include_tag *app.exposed_js_assets
     end
 
     def include_ember_stylesheet_tags(app_name)
       app = EmberCLI.configuration.apps.fetch(app_name)
-      stylesheet_link_tag *app.exposed_css_assets
+      ember_stylesheet_link_tag *app.exposed_css_assets
     end
+
+    def ember_asset_path(path)
+      if EmberCLI::Helpers.non_production?
+        [ Rails.configuration.assets.prefix, path ] * ?/
+      else
+        path
+      end
+    end
+
+    def ember_javascript_include_tag(*paths)
+      options = paths.extract_options!
+      javascript_include_tag(*paths.map(&method(:ember_asset_path)), options)
+    end
+
+    def ember_stylesheet_link_tag(*paths)
+      options = paths.extract_options!
+      stylesheet_link_tag(*paths.map(&method(:ember_asset_path)), options)
+    end
+
   end
 end
