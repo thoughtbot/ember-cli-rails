e444ed2e4762ce19f24f7a40d7913edc79c18f14
Jonathan Jackson <jonathan.jackson1@me.com>
2014-12-22 13:16:58 -0500

    Bump dependency on ember-cli-rails to 0.0.5
    This version of the addon includes a monkeypatch that properly fires the
    `preBuild` hook before build is invoked.
    
    fixes #31

--- lib/ember-cli/app.rb
+++ lib/ember-cli/app.rb
@@ -2,7 +2,7 @@ require "timeout"
 
 module EmberCLI
   class App
-    ADDON_VERSION = "0.0.3"
+    ADDON_VERSION = "0.0.5"
     EMBER_CLI_VERSION = "~> 0.1.3"
 
     attr_reader :name, :options, :pid
