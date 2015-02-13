a4c72743854d65d88ed79da534f89c3303694a59
Artur Roszczyk <artur.roszczyk@gmail.com>
2015-02-13 20:37:06 +0100

    Fix Pathname to String conversion

--- lib/ember-cli/app.rb
+++ lib/ember-cli/app.rb
@@ -242,7 +242,7 @@ module EmberCLI
       ENV.clone.tap do |vars|
         vars.store "DISABLE_FINGERPRINTING", "true"
         vars.store "EXCLUDE_EMBER_ASSETS", excluded_ember_deps
-        vars.store "BUNDLE_GEMFILE", gemfile_path
+        vars.store "BUNDLE_GEMFILE", gemfile_path.to_s
       end
     end
 
