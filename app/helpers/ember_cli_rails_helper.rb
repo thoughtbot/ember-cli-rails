335d117a5bf4d3520730c9c421a25aee5274c2b3
--local <pavel@pravosud.com>
2015-03-22 00:39:59 -0400

    Allow helpers to take options for ember assets

--- app/helpers/ember_cli_rails_helper.rb
+++ app/helpers/ember_cli_rails_helper.rb
@@ -1,9 +1,9 @@
 module EmberCLIRailsHelper
-  def include_ember_script_tags(name)
-    javascript_include_tag *EmberCLI[name].exposed_js_assets
+  def include_ember_script_tags(name, options={})
+    javascript_include_tag *EmberCLI[name].exposed_js_assets, options
   end
 
-  def include_ember_stylesheet_tags(name)
-    stylesheet_link_tag *EmberCLI[name].exposed_css_assets
+  def include_ember_stylesheet_tags(name, options={})
+    stylesheet_link_tag *EmberCLI[name].exposed_css_assets, options
   end
 end
