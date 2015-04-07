5bb761af7bd3771ef9adcce026f3f9ac4457e774
Weston Sewell <westons@spiceworks.com>
2015-04-06 23:16:50 -0500

    Added include_ember_alternative_stylesheet_tags() helper which lets you specify a custom stylesheet to pluck from ember and specify (eg) a 'media' attribute.

--- app/helpers/ember_rails_helper.rb
+++ app/helpers/ember_rails_helper.rb
@@ -6,4 +6,14 @@ module EmberRailsHelper
   def include_ember_stylesheet_tags(name, options={})
     stylesheet_link_tag *EmberCLI[name].exposed_css_assets, options
   end
+
+  def include_ember_alternative_stylesheet_tags(app_name, *sources)
+    app = EmberCLI[name]
+
+    sources = sources.map do |source_or_option|
+      (source_or_option.is_a? Hash) ? source_or_option : "#{app.name}/#{source_or_option}"
+    end
+
+    stylesheet_link_tag *sources
+  end
 end
