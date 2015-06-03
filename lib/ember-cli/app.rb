27eb39338cf05ec9beb399098a781aa0370904ac
Sean Doyle <sean.p.doyle24@gmail.com>
2015-06-03 18:53:08 -0400

    Forward `RAILS_ENV` to `ember build` shell
    Exposes `Rails.env` to EmberCLI's node process as `RAILS_ENV`.
    
    This can be helpful to detect the Rails environment from within the
    EmberCLI process.
    
    This can be useful to determine whether or not EmberCLI is running in
    its own standalone process or being managed by Rails.
    
    For example, to enable [ember-cli-mirage][ember-cli-mirage] API
    responses in `development` while being run outside of Rails (while run
    by `ember serve`), check for the absence of the `RAILS_ENV` environment
    variable:
    
    ```js
    // config/environment.js
    if (environment === 'development') {
      ENV['ember-cli-mirage'] = {
        enabled: typeof process.env.RAILS_ENV === 'undefined',
      }
    }
    ```
    
    `RAILS_ENV` will be absent in production builds.
    
    [ember-cli-mirage]: http://ember-cli-mirage.com/docs/latest/

--- lib/ember-cli/app.rb
+++ lib/ember-cli/app.rb
@@ -231,6 +231,7 @@ module EmberCLI
 
     def env_hash
       ENV.clone.tap do |vars|
+        vars.store "RAILS_ENV", Rails.env
         vars.store "DISABLE_FINGERPRINTING", "true"
         vars.store "EXCLUDE_EMBER_ASSETS", excluded_ember_deps
         vars.store "BUNDLE_GEMFILE", gemfile_path.to_s if gemfile_path.exist?
