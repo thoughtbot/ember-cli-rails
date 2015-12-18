942e6eeb8cdbfcb0b7f59d6f58f562b3a7c72b60
Sean Doyle <sean.p.doyle24@gmail.com>
2015-12-18 17:39:15 -0500

    Remove Asset code

--- app/helpers/ember_rails_helper.rb
+++ app/helpers/ember_rails_helper.rb
@@ -1,5 +1,4 @@
 require "html_page/capture"
-require "ember_cli/assets"
 
 module EmberRailsHelper
   def render_ember_app(name, &block)
@@ -10,25 +9,5 @@ module EmberRailsHelper
     head, body = markup_capturer.capture
 
     render inline: EmberCli[name].index_html(head: head, body: body)
-  end
-
-  def include_ember_script_tags(name, **options)
-    EmberCli[name].build
-
-    assets = EmberCli::Assets.new(EmberCli[name])
-
-    assets.javascript_assets.
-      map { |src| %{<script src="#{src}"></script>}.html_safe }.
-      reduce(&:+)
-  end
-
-  def include_ember_stylesheet_tags(name, **options)
-    EmberCli[name].build
-
-    assets = EmberCli::Assets.new(EmberCli[name])
-
-    assets.stylesheet_assets.
-      map { |src| %{<link rel="stylesheet" href="#{src}">}.html_safe }.
-      reduce(&:+)
   end
 end
