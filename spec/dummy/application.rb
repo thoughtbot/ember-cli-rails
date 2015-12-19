033e997bb7712a3aad36a2ab9d4eae978e4bcb3d
Sean Doyle <sean.p.doyle24@gmail.com>
2015-12-18 21:30:54 -0500

    Remove Sprockets Configuration from Dummy app
    Since we no longer depend on Sprockets and the Asset Pipeline, `asset`
    configuration is no longer necessary, and only adds noise to the
    configuration.

--- spec/dummy/application.rb
+++ spec/dummy/application.rb
@@ -2,7 +2,6 @@ require "rails"
 
 require "action_controller/railtie"
 require "action_view/railtie"
-require "sprockets/railtie"
 
 Bundler.require(*Rails.groups)
 
@@ -23,11 +22,6 @@ module Dummy
 
     # Print deprecation notices to the stderr.
     config.active_support.deprecation = :stderr
-
-    config.assets.enabled = true
-    config.assets.digest = true
-    config.assets.raise_runtime_errors = true
-    config.assets.version = "1.0"
 
     config.secret_token = "SECRET_TOKEN_IS_MIN_30_CHARS_LONG"
     config.secret_key_base = "SECRET_KEY_BASE"
