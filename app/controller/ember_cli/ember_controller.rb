2f58783ef2768decfd9cc42263504a886a8ceb93
Sean Doyle <sean.p.doyle24@gmail.com>
2016-07-08 16:22:21 -0400

    render :text

--- app/controller/ember_cli/ember_controller.rb
+++ app/controller/ember_cli/ember_controller.rb
@@ -1,12 +1,5 @@
 module EmberCli
   class EmberController < ::ApplicationController
-    # (
-    #   ::ActionController::Base::MODULES -
-    #   ::ActionController::API::MODULES
-    # ).each do |mod|
-    #   include mod
-    # end
-    #
     def index
       render layout: false
     end
