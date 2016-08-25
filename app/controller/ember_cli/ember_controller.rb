d1498e1811494ac04242fc4fe1fcd2f6a1d982ab
Sean Doyle <sean.p.doyle24@gmail.com>
2016-08-25 10:41:01 -0400

    Add modules to `ember_cli/ember` controller
    Closes [#480].
    
    When the `ApplicationController` extends `ActionController::API`, HTML
    rendering modules and logic are omitted.
    
    To make the `EmberCli::EmberController` capable of rendering the Ember
    application's HTML, this commit ensures those modules are included when
    necessary.
    
    [#480]: https://github.com/thoughtbot/ember-cli-rails/issues/480

--- app/controller/ember_cli/ember_controller.rb
+++ app/controller/ember_cli/ember_controller.rb
@@ -1,5 +1,16 @@
 module EmberCli
   class EmberController < ::ApplicationController
+    unless ancestors.include? ActionController::Base
+      (
+        ActionController::Base::MODULES -
+        ActionController::API::MODULES
+      ).each do |controller_module|
+        include controller_module
+      end
+
+      helper EmberRailsHelper
+    end
+
     def index
       render layout: false
     end
